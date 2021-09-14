4a349b1503b21e0c87438d5030918bd03a1c4e59
Rashaun Ellison <rellison27@gmail.com>
2021-09-14 07:42:17 -0400

    cleaning tree

--- shared/src/commonMain/kotlin/com/example/breezepoc/datasource/network/PeopleServiceImpl.kt
+++ shared/src/commonMain/kotlin/com/example/breezepoc/datasource/network/PeopleServiceImpl.kt
@@ -61,7 +61,7 @@ singlePhoneNetworkMapper,
     }
 
     companion object {
-        const val BEARER = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGkuYnJlZXplY2htcy5jb21cL2FwaVwvdjJcL2F1dGhcL2xvZ2luIiwiaWF0IjoxNjMxNTM5Mjc3LCJleHAiOjE2MzE1NDI4NzcsIm5iZiI6MTYzMTUzOTI3NywianRpIjoiZG9SS2VLcGg0V0c4cENjTyIsInN1YiI6Nzc1MTc2LCJwcnYiOiI0YWMwNWMwZjhhYzA4ZjM2NGNiNGQwM2ZiOGUxZjYzMWZlYzMyMmU4In0.Z0MKTj_D5WinA_NQr_JweucNJWSpwHD6F_8IrFXyd0w"
+        const val BEARER = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGkuYnJlZXplY2htcy5jb21cL2FwaVwvdjJcL2F1dGhcL2xvZ2luIiwiaWF0IjoxNjMxNTU1NzUxLCJleHAiOjE2MzE1NTkzNTEsIm5iZiI6MTYzMTU1NTc1MSwianRpIjoiQk50dUtlMlR3YnliUmdGYiIsInN1YiI6Nzc1MTc2LCJwcnYiOiI0YWMwNWMwZjhhYzA4ZjM2NGNiNGQwM2ZiOGUxZjYzMWZlYzMyMmU4In0.HdYoQgBxUkheTsvhzJiYppJcEsbZycK6Ev3-dRAeqRE"
         const val BASE_URL = "https://api.breezechms.com/api/v2/people"
         const val PEOPLE_PARAMS = "?sort=[formalName:asc]&filter[is_archived:eq:boolean]=false&filter[email:contains:text]=@,0,0&filter[phone:contains:text]=@:Mobile:false:false"
     }

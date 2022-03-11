00180b2f6edc94be20d7c26e026ad063ac67552b
Rashaun Ellison <rellison27@gmail.com>
2022-03-11 14:47:09 -0500

    using personal prod account and iOS client had issue with reassignment of Bearer var

--- shared/src/commonMain/kotlin/com/example/breezepoc/datasource/network/PeopleServiceImpl.kt
+++ shared/src/commonMain/kotlin/com/example/breezepoc/datasource/network/PeopleServiceImpl.kt
@@ -51,14 +51,14 @@ class PeopleServiceImpl(
         val response: AuthResponse = httpClient.post<AuthResponse> {
             url("https://api.breezechms.com/api/v2/auth/login")
             contentType(ContentType.Application.Json)
-            body = Auth("demo","demo","demo")
+            body = Auth("demo","demo","rashaune")
         }
         val mappedToken = tokenMapper.mapToDomainModel(response)
         println("Token $mappedToken")
-        BEARER = "Bearer ${mappedToken.accessToken}"
+        val bearerToken = "Bearer ${mappedToken.accessToken}"
         val people = httpClient.get<PeopleResponse> {
             url("$BASE_URL/$PEOPLE_PARAMS")
-            header("Authorization", BEARER,)
+            header("Authorization", bearerToken,)
         }.data
         val mapped = peopleNetworkMapper.mapToDomainList(people)
         println("Testing123 $mapped")
@@ -77,7 +77,8 @@ class PeopleServiceImpl(
     }
 
     companion object {
-         var BEARER = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGkuYnJlZXplY2htcy5jb21cL2FwaVwvdjJcL2F1dGhcL2xvZ2luIiwiaWF0IjoxNjQ2MjMwMTI5LCJleHAiOjE2NDYyMzM3MjksIm5iZiI6MTY0NjIzMDEyOSwianRpIjoiTE9zUXB0NkhNREI2dFlobCIsInN1YiI6MzU1MzQ2LCJwcnYiOiI0YWMwNWMwZjhhYzA4ZjM2NGNiNGQwM2ZiOGUxZjYzMWZlYzMyMmU4In0.cbMYgdc9e5cvDTwjOtj5iW_ckDz2xnT8p1J0Y13_yjA"
+        // old manual way of logging in
+         var BEARER = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGkuYnJlZXplY2htcy5jb21cL2FwaVwvdjJcL2F1dGhcL2xvZ2luIiwiaWF0IjoxNjQ3MDI2MDIyLCJleHAiOjE2NDcwMjk2MjIsIm5iZiI6MTY0NzAyNjAyMiwianRpIjoialV2S3pRR2xPamtCMnlDbSIsInN1YiI6Nzc1MTc2LCJwcnYiOiI0YWMwNWMwZjhhYzA4ZjM2NGNiNGQwM2ZiOGUxZjYzMWZlYzMyMmU4In0.156kx3R1t6k6Vn0gAO3ZJvN8p3V1sLhjns8_cRSwCRU"
         const val BASE_URL = "https://api.breezechms.com/api/v2/people"
         const val PEOPLE_PARAMS = "replacement?filter[or]=(filter[phone.mobile.number:exists]=true,filter[email.address:exists]=true,filter[birthdate:exists]=true,filter[family.role:exists]=true,filter[address.street:exists]=true)&filter[archived:exists]=false&sort=[last_name:asc,first_name:asc]"
     }
